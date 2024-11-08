9c17a122579b3e5d90cf0f60476b0620e0ae70bb
Cheida <61758362+Cheida@users.noreply.github.com>
2024-11-08 15:31:08 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,69 +1,119 @@
 #include "funcao.h"
 
-int salvar_dados(cadastro *p){
-    FILE *arquivo = fopen("usuarios.txt", "w");
+int salvar_dados(cadastro *p) {
+  FILE *arquivo = fopen("usuarios.txt", "w");
 
-    if(arquivo == NULL){
-        printf("Erro ao abrir o arquivo!\n");
-        return 1;
-    }
+  if (arquivo == NULL) {
+    printf("Erro ao abrir o arquivo!\n");
+    return 1;
+  }
 
-	int i;
-    for(i = 0; i < 10; i++){   // Salvar os dados atualizados no arquivo
-        fprintf(arquivo, "%s;%s;%s;%s;%s;%s;%s\n", p[i].cpf, p[i].nome, p[i].senha, p[i].reais, p[i].bitcoin, p[i].Ethereum, p[i].Ripple);
-        //printf("%s;%s;%s;%s;%s;%s;%s\n", p[i].cpf, p[i].nome, p[i].senha, p[i].reais, p[i].bitcoin, p[i].Ethereum, p[i].Ripple); -- teste
-    }
+  int i;
+  for (i = 0; i < 10; i++) { // Salvar os dados atualizados no arquivo
+    fprintf(arquivo, "%s;%s;%s;%s;%s;%s;%s\n", p[i].cpf, p[i].nome, p[i].senha,
+            p[i].reais, p[i].bitcoin, p[i].Ethereum, p[i].Ripple);
+  }
 
-    fclose(arquivo);
-    return 0;
+  fclose(arquivo);
+  return 0;
 }
 
 int menu(cadastro *p, int index) {
-    int resposta;
-    while (1) {
-        printf("----------------------------\n");
-        printf("| 1. Consultar saldo       |\n");
-        printf("| 2. Consultar extrato     |\n");
-        printf("| 3. Depositar             |\n");
-        printf("| 4. Sacar                 |\n");
-        printf("| 5. Comprar criptomoeda   |\n");
-        printf("| 6. Vender criptomoedas   |\n");
-        printf("| 7. Atualizar cripto      |\n");
-        printf("| 8. Sair                  |\n");
-        printf("----------------------------\n");
-        scanf("%d", &resposta);
-        getchar();
+  int resposta;
+  while (1) {
+    printf("----------------------------\n");
+    printf("| 1. Consultar saldo       |\n");
+    printf("| 2. Consultar extrato     |\n");
+    printf("| 3. Depositar             |\n");
+    printf("| 4. Sacar                 |\n");
+    printf("| 5. Comprar criptomoeda   |\n");
+    printf("| 6. Vender criptomoedas   |\n");
+    printf("| 7. Atualizar cripto      |\n");
+    printf("| 8. Sair                  |\n");
+    printf("----------------------------\n");
+    scanf("%d", &resposta);
+    getchar();
 
-        switch (resposta) {
-            case 1:
-                Consultarsaldo(p, index);
-                break;
-            case 2:
-                Consultarextrato(p, index);
-                break;
-            case 3:
-                Depositar(p, index);
-                salvar_dados(p);
-                break;
-            case 4:
-                Sacar(p, index);
-                salvar_dados(p);
-                break;
-            case 5:
-                comprarcripto(p, index);
-                salvar_dados(p);
-                break;
-            case 6:
-                vendercripto(p, index);
-                salvar_dados(p);
-                break;
-            case 7:
-                Atualizar(); // atualizar os valores das criptomoedas
-                break;
-            case 8:
-                return 0;  // Sair do menu
-            default:
-                printf("Essa opção não é válida!\n");
-        }
+    switch (resposta) {
+    case 1:
+      Consultarsaldo(p, index);
+      break;
+    case 2:
+      Consultarextrato(p, index);
+      break;
+    case 3:
+      Depositar(p, index);
+      salvar_dados(p);
+      break;
+    case 4:
+      Sacar(p, index);
+      salvar_dados(p);
+      break;
+    case 5:
+      comprarcripto(p, index);
+      salvar_dados(p);
+      break;
+    case 6:
+      vendercripto(p, index);
+      salvar_dados(p);
+      break;
+    case 7:
+      Atualizar(); // atualizar os valores das criptomoedas
+      break;
+    case 8:
+      return 0; // Sair do menu
+    default:
+      printf("Essa opcao nao e valida!\n");
     }
+  }
 }
+int menu_admin() {
+  int resposta;
+  while (1) {
+      printf("|======================================|\n");
+      printf("|1. Cadastrar Novo Investidor          |\n");
+      printf("|2. Excluir Investidor                 |\n");
+      printf("|3. Cadastrar de Criptomoeda           |\n");
+      printf("|4. Excluir Criptomoeda                |\n");
+      printf("|5. Consultar Saldo de um Investidor   |\n");
+      printf("|6. Consultar Extrato de um Investidor |\n");
+      printf("|7. Atualizar cotaÃ§Ã£o de criptomoedas  |\n");
+      printf("|8. Sair                               |\n");
+      printf("|======================================|\n");
+      scanf("%d", &resposta);
+      getchar();
+
+      switch (resposta) {
+          case 1:
+              printf("Cadastrar Novo Investidor\n");
+              break;
+          case 2:
+              printf("Excluir Investidor\n");
+              break;
+          case 3:
+              printf("Cadastrar de Criptomoeda\n");
+              break;
+          case 4:
+              printf("Excluir Criptomoeda\n");
+              break;
+          case 5:
+              printf("Consultar Saldo de um Investidor\n");
+              break;
+          case 6:
+              printf("Consultar Extrato de um Investidor\n");
+              break;
+          case 7:
+              printf("Atualizar cotaÃ§Ã£o de criptomoedas\n");
+              break;
+          case 8:
+              break;
+          default:
+              printf("| OpÃ§Ã£o InvÃ¡lida!!! |\n");
+      }
+      if (resposta == 8){
+      return 0;
+    }
+  }
+}  
+  
+
